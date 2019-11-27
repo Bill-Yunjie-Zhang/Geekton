70606bee5280e5f43068a3c22eea6c2571f03678
Bill <207559339@qq.com>
2019-11-27 09:47:11 +0800

    2019.11.27

--- data/events.js
+++ data/events.js
@@ -7,7 +7,7 @@ var events = [{
     },
     trailer: "After the final exam, receiving the reports of performance and looking at the scarlet crosses on the papers, many members of our Geekton community expressed bitter complaints about how inhumane the test papers are and how brutal our educational system is. In fact, every student in mainland are sharing exactly the same feeling, and we believe organizing a forum on this topic to be a perfect opportunity for the students’ emotional catharsis and critical thinking ability. During our discussion, we all complained about the rigid curricula commandeering our spare time, misleading materials in our textbooks corroding our minds, and Gaokao system stifling our creativity and judgement. On the other hand, we failed to find a better way, at least a fairer way to fit in our current condition: an excessively populated nation with severely limited resources. A well-written article summarizing our dissatisfaction and explaining why Gaokao the only solution regarding current conditions is now available on our WeChat official account.",
     img: "pics/educational.png",
-    linkToAssay: "www.baidu.com"
+    linkToAssay: "https://mp.weixin.qq.com/s?__biz=MzI5ODkzMjE3MA==&mid=2247483696&idx=1&sn=c2b2ce6bc940e04f134ecdb027bb673a&chksm=ec9f0395dbe88a836073a88ae0b5802e49452122f48ff1ac070b16ec504b19e1eafcb329fbae&scene=0&xtrack=1&key=b904296be8cb5e6787626ca1453e9f9eb3bbe68c17dbcdceb3363f0b30a768d0b572c9b5e655ec3298c83dd2590fa7cc07f62dc81cb1865b68194a9f9f200536e03e59e1d509d27dd788c05f87beb45e&ascene=1&uin=ODEyNjA2Nzky&devicetype=Windows+10&version=62070152&lang=zh_CN&pass_ticket=hliD4u9AGiKK%2F%2FTK2bvI%2FkrNQEgg%2FAlhGVoJRo3KAOKKIih1N9UNzYuWk8gJ7ViP"
 },{
     topic: "The trade war & Huawei",
     date: {
@@ -17,7 +17,7 @@ var events = [{
     },
     trailer: "As the US-China trade war intensifies and our peers are deeply infused with a radically patriotic idea, we, founders of Geekton, feel it necessary to find out the real cause of this war and all of its latent consequences in order to promulgate a more objective and rational understanding about this event as well as the way this world around us works. Before the forum, we spent days reading and filtering articles on the tariff war and further developments from varied sources and standpoints, and sent them to all our attenders, who used hold prejudiced stances. Surprisingly, during our intensive discussion in the forum, we heard assorted perspectives not only from chauvinists but also from pessimist, who expressed their deepest concern about the future of our country, the biggest growing economy. After the forum, the Geekton team wrote an article explaining the origin of the conflicts and compiling the opinions from every party and every single, and made a few predictions about the tariff war, which is currently available on our WeChat official account.",
     img: "pics/huawei.png",
-    linkToAssay: "www.baidu.com"
+    linkToAssay: "https://mp.weixin.qq.com/s?__biz=MzI5ODkzMjE3MA==&mid=2247483716&idx=1&sn=6c3bc86dabf211d6ae9be7144326270b&chksm=ec9f03e1dbe88af7412ec00cfe24ab7ca05ba4625af2dbce509943256c0bdc7bd7b3fdd8d46a&scene=0&xtrack=1&key=a784f15945931fc07b96bae57b35794fca9a40dd60603ea79f280cd2fdc9486d891e7d1bb2d90954cc33fe4219dc96149aaea9418ea8b5d9782844c95c362b7f793baa9c3f41f45031689c798312e738&ascene=1&uin=ODEyNjA2Nzky&devicetype=Windows+10&version=62070152&lang=zh_CN&pass_ticket=hliD4u9AGiKK%2F%2FTK2bvI%2FkrNQEgg%2FAlhGVoJRo3KAOKKIih1N9UNzYuWk8gJ7ViP"
 },{
     topic: "Psyche 16: heaven or hell",
     date: {
@@ -27,7 +27,7 @@ var events = [{
     },
     trailer: "NASA announced the Psyche Mission—provoking shock and criticism among the international community—to understand planet formation by exploring firsthand a brand- new and uncharted corner of the cosmos, and most importantly, to bring back the gold and other rare metals on the asteroid. Its various metals are worth a gargantuan $10,000 quadrillion. Seemingly, carrying all those metals back to earth is capable of making every individual a billionaire. However, our discussion focused more on the negative effects of the excavation: quadrillions of tons of gold and platinum might just destroy commodity prices and cause the world's economy – worth $75.5 trillion – to collapse completely. When we think twice about NASA’s decision, a series of contradictions about this mission will tell us that this is actually not practical at all, and doubts were put forward in our meeting along with fierce discussions……",
     img: "pics/pyche.png",
-    linkToAssay: "www.baidu.com"
+    linkToAssay: "http://3152828959.qzone.qq.com"
 },{
     topic: "Boeing 737 Max being grounded",
     date: {
@@ -37,7 +37,7 @@ var events = [{
     },
     trailer: "The world was stricken with tremendous grief and the entire air industry was shocked by the biggest security problem so far, as a series of air crash of Boeing 737 Max8 was reported on TV. While we lament the tragic deaths of every victim, the very malfunction which pulls the trigger has also intrigued us. So, our community collected a bunch of analysis papers as well as official accident reports which contain more than a hundred pages. As we went through all the articles, we are actually informed with several distinct theories accounting for possible causes. During our discussion, we drew a few models for the causes, but failed to find any flaw in each of them. Also, we made reasonable predictions about the economic and political consequences of such wrecks, and came up with feasible plans against further deterioration.",
     img: "pics/Boeing.png",
-    linkToAssay: "www.baidu.com"
+    linkToAssay: "http://3152828959.qzone.qq.com"
 },{
     topic: "The Heat Wave",
     date: {
@@ -47,5 +47,25 @@ var events = [{
     },
     trailer: "The heat wave, not familiar to many, is a pressing crisis faced by every individual on this planet. Though, as the name suggests, you might consider this problem to be no big deal other than higher temperatures which can be readily solved by air conditioners, while the reality far beyond your imagination, and can lead to catastrophic consequences. For example, the heat wave might expose the crops to latent hyperthermia, which is highly likely to cause massive crop failures, and also compel us to resort to air conditioners more frequently, which will, as an inevitable result, aggravate the power shortages. Whereas,  In the light of this extreme weather can be deadly, our community collected sufficient information to make the problem more intelligible. During our conference, not only did everyone realized the severity of the situation, but also analyzed the potential economic and environmental effects.",
     img: "pics/heatwave.png",
-    linkToAssay: "www.baidu.com"
+    linkToAssay: "http://3152828959.qzone.qq.com"
+},{
+    topic: '"ABC" 专访',
+    date: {
+        year:2019,
+        month:11,
+        day:30
+    },
+    trailer: 'ABC（American-Born Chinese），最初意指出生在美国的华裔，后来也指二代移民或者三代移民。他们与华人一样有黄皮肤黑头发，但因为从小接受外国教育，思想观念会有所不同。而我们采访到的这位"ABC"，是一名澳大利亚籍的华裔姑娘，Carmen。想要听她的故事，想要了解华裔的海外生活，请根据海报提供的地点与时间来加入我们的讨论吧。',
+    img: "pics/ABC.png",
+    linkToAssay: "https://mp.weixin.qq.com/s?__biz=MzI5ODkzMjE3MA==&mid=2247483748&idx=1&sn=c5f235144a73fe37a343958b1d89a574&chksm=ec9f03c1dbe88ad7663273ade486d6413f9aec90b691b19603d4b8245e9a6329788eca44bc7e&mpshare=1&scene=1&srcid=&sharer_sharetime=1574777227112&sharer_shareid=e364e9e49e855c7bb4a1379ee10f00a0&key=8b842e16f8b11c97ac4d215b3cbde1a074bab23ee6a6505a0830ba7c51629207ebe23747e7758519b074756b2b54d349bd181e4da932064cedfba13832736cecaf6c6877051c3be449c6359e6524a5ae&ascene=1&uin=ODEyNjA2Nzky&devicetype=Windows+10&version=62070152&lang=zh_CN&pass_ticket=hliD4u9AGiKK%2F%2FTK2bvI%2FkrNQEgg%2FAlhGVoJRo3KAOKKIih1N9UNzYuWk8gJ7ViP"
+},{
+    topic: "跟随成教授探索干细胞",
+    date: {
+        year:2019,
+        month:12,
+        day:28
+    },
+    trailer: "本次活动中，陈国凯教授将会为我们简述他在美国的求学历程——怎样下定决心到一个完全陌生的环境，怎样取得今天瞩目的成就，同时为我们讲授基础的干细胞知识以及它在生物医学中的应用。在讲座之后会有提问与自由讨论环节，同学们可以向陈教授提出任何与学术或是留学相关的问题，亦可与其余在座的与会者一起讨论对讲座内容的疑虑或更多的见解。当然，陈教授也会参与到大家的讨论当中并为我们提供学术支持。Geekton举办本次特别会议旨在让我们的同龄人接触最前沿的科学，接触到教材无法覆盖却又不可或缺的知识，同时锻炼批判性思维与语言表达能力。希望同学们都能积极参与活动，踊跃报名。",
+    img: "pics/Biology.png",
+    linkToAssay: "https://mp.weixin.qq.com/s?__biz=MzI5ODkzMjE3MA==&mid=2247483736&idx=1&sn=9777d977456fe261d6aa424643f030e3&chksm=ec9f03fddbe88aebb5803f38bfe8402918852d17fb1d7406e669c4c17925112b211689f92855&mpshare=1&scene=1&srcid=&sharer_sharetime=1574777220426&sharer_shareid=e364e9e49e855c7bb4a1379ee10f00a0&key=a784f15945931fc027ff9634c0d6c1663728ad3b75d08824e8304bf74a3c38be0c1b5ac15664e826cf1e9e5464e087e83d6caa95fdcc9e4fd9ab16a8e2a6eff9866635bda62171e93e1d8a37b92ef24c&ascene=1&uin=ODEyNjA2Nzky&devicetype=Windows+10&version=62070152&lang=zh_CN&pass_ticket=hliD4u9AGiKK%2F%2FTK2bvI%2FkrNQEgg%2FAlhGVoJRo3KAOKKIih1N9UNzYuWk8gJ7ViP"
 }]
